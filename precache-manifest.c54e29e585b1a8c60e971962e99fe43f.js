2cae0bcdb5aa044bb8e18fea36fe9ffe08913827
Matik, Thomas T <thomas-matik@uiowa.edu>
2020-06-02 09:10:25 -0500

    Updates

---
+++ precache-manifest.c54e29e585b1a8c60e971962e99fe43f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a32ad94bad208fadb3ce270d53914e8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e4e31b9ae15e041e918f",
+    "url": "/robofriends/static/css/main.82a5450c.chunk.css"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e31b9ae15e041e918f",
+    "url": "/robofriends/static/js/main.32b385b0.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
