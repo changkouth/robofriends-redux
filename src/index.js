07c0b950273c776d6d3b1ba6df8c44b861fb0594
Matik, Thomas T <thomas-matik@uiowa.edu>
2020-06-04 11:42:35 -0500

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
+
+import App from './containers/App';
+import { requestRobots, searchRobots } from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const logger = createLogger();
+
+const rootReducers = combineReducers({ requestRobots, searchRobots});
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 serviceWorker.unregister();
 
